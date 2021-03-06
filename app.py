c6a2c6970565e2d69da8a959c158fe8ce64348dc
fernandagil <fernandagilgonzalez@gmail.com>
2021-03-06 12:29:52 +0000

    Add Bootstrap Navbar and create new add_review.html file

--- app.py
+++ app.py
@@ -26,6 +26,11 @@ def get_reviews():
     return render_template("reviews.html", movies=movies)
 
 
+@app.route("/add_review")
+def add_review():
+    return render_template("add_review.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
