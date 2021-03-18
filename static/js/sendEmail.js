60639af96454593a5589097c5505b96b72b1d8af
fernandagil <fernandagilgonzalez@gmail.com>
2021-03-18 16:56:24 +0000

    Implement emailjs integration on the register.html template

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,3 +1,19 @@
+function sendWelcomeEmail(loginForm) {
+    emailjs.send("gmail", "signinConfirmation", {
+        "to_name": loginForm.username.value,
+        "to_email": loginForm.email.value,
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+
+}
+
 function sendMail(contactForm) {
     emailjs.send("gmail", "movieSuggestion", {
         "to_name": contactForm.name.value,
